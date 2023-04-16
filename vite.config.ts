899722b50cf9921c7e5704a1276b16bde73dd634
mioe <mioe.boss@gmail.com>
2023-04-16 10:24:16 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ command, mode }) => {
 	console.log('🦕 vite.config.ts/defineConfig', command, mode)
 
 	return {
-		base: mode === 'development' ? './' : '/playground/',
+		base: mode === 'development' ? './' : '/use-draggable/',
 
 		resolve: {
 			alias: {
