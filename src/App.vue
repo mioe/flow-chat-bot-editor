8de41e0114015f53d3cd05f1182b37b41b1f7ef8
mioe <mioe.boss@gmail.com>
2023-04-16 10:41:41 +0300

    reset base layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-const routes = router.options.routes
-</script>
-
 <template>
-	<header class="flex gap-[16px] p-[8px]">
-		<RouterLink
-			v-for="route in routes"
-			:key="route.name"
-			:to="{ name: route.name }"
-		>
-			{{ route.name }}
-		</RouterLink>
-	</header>
-
-	<RouterView />
+	<div class="flex flex-col">
+		<header class="shrink-0 w-full bg-[#222121] p-[16px]">
+			HEADER
+		</header>
+		<main class="w-full h-full flex">
+			<aside class="shrink-0 w-[300px] h-[calc(100vh-56px)] bg-[#1b1b1b] p-[16px]">
+				LEFT ASIDE
+			</aside>
+			<section class="w-full h-[calc(100vh-56px)]">
+				<RouterView />
+			</section>
+		</main>
+	</div>
 </template>
