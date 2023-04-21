1538b521d587caed8aea4c74b05d0df1d0e332cc
mioe <mioe.boss@gmail.com>
2023-04-21 14:39:21 +0300

    add more styles

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 	<div class="flex flex-col">
 		<header class="shrink-0 w-full h-[64px] bg-$ui-white p-[16px] border-0 border-b border-solid border-$deviders-primary" />
 		<main class="w-full h-full flex">
-			<aside class="shrink-0 w-[56px] h-[calc(100vh-56px)] bg-$ui-white p-[16px] border-0 border-r border-solid border-$deviders-primary" />
-			<section class="w-full h-[calc(100vh-56px)] flex flex-col">
+			<aside class="shrink-0 w-[56px] h-[calc(100vh-64px)] bg-$ui-white p-[16px] border-0 border-r border-solid border-$deviders-primary" />
+			<section class="w-full h-[calc(100vh-64px)] flex flex-col">
 				<RouterView />
 			</section>
 		</main>
