0a58a5199a0b20355e23989055e7bf491cc81a7b
mioe <mioe.boss@gmail.com>
2023-05-02 20:56:38 +0300

    remove ignores rule

--- src/components/ChatBots/CustomNodeRenderer.ts
+++ src/components/ChatBots/CustomNodeRenderer.ts
@@ -1,21 +1,13 @@
 import { ComponentOptions, defineComponent, h } from 'vue'
 import { Components } from '@baklavajs/renderer-vue'
 import { AbstractNode } from '@baklavajs/core'
-// @ts-ignore
 import StartNodeRenderer from '~/components/ChatBots/ChatBotsNodes/StartNodeRenderer.vue'
-// @ts-ignore
 import ActionNodeRenderer from '~/components/ChatBots/ChatBotsNodes/ActionNodeRenderer.vue'
-// @ts-ignore
 import IdleNodeRenderer from '~/components/ChatBots/ChatBotsNodes/IdleNodeRenderer.vue'
-// @ts-ignore
 import IfNodeRenderer from '~/components/ChatBots/ChatBotsNodes/IfNodeRenderer.vue'
-// @ts-ignore
 import MessageNodeRenderer from '~/components/ChatBots/ChatBotsNodes/MessageNodeRenderer.vue'
-// @ts-ignore
 import InputNodeRenderer from '~/components/ChatBots/ChatBotsNodes/InputNodeRenderer.vue'
-// @ts-ignore
 import RedirectNodeRenderer from '~/components/ChatBots/ChatBotsNodes/RedirectNodeRenderer.vue'
-// @ts-ignore
 import TemplateWabaNodeRenderer from '~/components/ChatBots/ChatBotsNodes/TemplateWabaNodeRenderer.vue'
 
 const NODE_TYPES = new Map()
